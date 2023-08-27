9f0afaa8646d7b08a9f503d955f170ad50af7074
Dias Sadykov <dias.sadykov0@gmail.com>
2023-08-27 19:38:19 +0300

    lego

--- src/utils/salary.py
+++ src/utils/salary.py
@@ -3,15 +3,16 @@ import re
 def convert_salary_to_int(salary: str) -> int:
     if not salary:
         return 0
-    
-    salary = salary.replace(' ', '')
-    salary = salary.replace('\xa0', '')
-    salary = salary.replace('\u202f', '')
-    numbers = re.findall(r'\d+', salary)
-    average_salary = sum(map(int, numbers)) // len(numbers)
-    currency_symbol = salary[-1]
-    currency_rates = {'₸': 0.0021, '₽': 0.011, '€': 1.17}
-    if currency_symbol in currency_rates:
-        average_salary *= currency_rates[currency_symbol]
-
-    return average_salary
+    try:
+        salary = salary.replace(' ', '')
+        salary = salary.replace('\xa0', '')
+        salary = salary.replace('\u202f', '')
+        numbers = re.findall(r'\d+', salary)
+        average_salary = sum(map(int, numbers)) // len(numbers)
+        currency_symbol = salary[-1]
+        currency_rates = {'₸': 0.0021, '₽': 0.011, '€': 1.17}
+        if currency_symbol in currency_rates:
+            average_salary *= currency_rates[currency_symbol]
+        return average_salary
+    except Exception as e:
+        return 0
