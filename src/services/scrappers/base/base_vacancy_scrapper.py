960bc3c998cb62a59c11aba0fc312c258364cf03
Dias Sadykov <dias.sadykov0@gmail.com>
2023-09-01 12:53:39 +0300

    lego

--- src/services/scrappers/base/base_vacancy_scrapper.py
+++ src/services/scrappers/base/base_vacancy_scrapper.py
@@ -19,6 +19,7 @@ class VacancyScrapperBase:
         while next_run_time < datetime.now():
             next_run_time = next_run_time + timedelta(hours=self.interval_hours)
         self.next_run_time = next_run_time
+        print(self.next_run_time, self.next_run_time.tzinfo)
 
     async def scrap(self) -> list[Vacancy]:
         # take all vacancies from db in a set and then compare with new vacancies
