74344569c2259da97aeadcd7d53cea5ae531134e
Dias Sadykov <dias.sadykov0@gmail.com>
2023-09-02 18:38:07 +0300

    lego

--- src/cron/scrapping_cron.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import time
-from services.scrappers.vacancy_scrappers import ALL_CHECKERS, ALL_SCRAPPERS
-
-async def run_scrapping():
-    await asyncio.gather(*[scrapper.run() for scrapper in ALL_SCRAPPERS+ALL_CHECKERS])
-
-
-
--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,9 @@
 import asyncio
 from db.utils import create_db_if_not_exists
-from cron.scrapping_cron import run_scrapping
+from services.scrappers.vacancy_scrappers import ALL_CHECKERS, ALL_SCRAPPERS
+
+async def run_scrapping():
+    await asyncio.gather(*[scrapper.run() for scrapper in ALL_SCRAPPERS+ALL_CHECKERS])
 
 if __name__ == "__main__":
     create_db_if_not_exists()
