e42f6758633fda1f852e2298e7c820b9095ada34
Dias Sadykov <dias.sadykov0@gmail.com>
2023-09-10 04:36:35 +0300

    lego

--- src/db/utils.py
+++ src/db/utils.py
@@ -6,7 +6,7 @@ DATABASE_LOCATION = os.environ.get("DATABASE_LOCATION") or "db.sqlite3"
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 if DB_USER and DB_PASSWORD:
-    engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/postgres", echo=True)
+    engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/techhunterdb", echo=True)
 else:
     engine = create_engine(f"sqlite:///{DATABASE_LOCATION}", echo=True, connect_args={'check_same_thread': False})
 
