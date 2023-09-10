cdbe1cb154433a5b7b9d5cbff619d2f7e465036e
Dias Sadykov <dias.sadykov0@gmail.com>
2023-09-10 04:30:24 +0300

    lego

--- src/db/utils.py
+++ src/db/utils.py
@@ -3,7 +3,12 @@ import os
 from sqlmodel import Session, create_engine
 
 DATABASE_LOCATION = os.environ.get("DATABASE_LOCATION") or "db.sqlite3"
-engine = create_engine(f"sqlite:///{DATABASE_LOCATION}", echo=True, connect_args={'check_same_thread': False})
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+if DB_USER and DB_PASSWORD:
+    engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/postgres", echo=True)
+else:
+    engine = create_engine(f"sqlite:///{DATABASE_LOCATION}", echo=True, connect_args={'check_same_thread': False})
 
 def get_session():
     with Session(engine) as session:
