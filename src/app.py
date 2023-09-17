19225df74d7d7bb8d060352e39911565f8b44ab7
Dias Sadykov <dias.sadykov0@gmail.com>
2023-09-17 14:59:56 +0300

    lego

--- src/app.py
+++ src/app.py
@@ -23,4 +23,4 @@ app.include_router(vacancy.router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True if ENV == "DEV" else False)
--- src/db/utils.py
+++ src/db/utils.py
@@ -4,7 +4,8 @@ from sqlmodel import Session, create_engine
 
 DB_USER = os.environ.get("DB_USER", "postgres")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
-engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@localhost:5432/techhunterdb", echo=True)
+ENV = os.environ.get("ENV") or "DEV"
+engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@localhost:5432/techhunterdb", echo=True if ENV == "DEV" else False)
 
 def get_session():
     with Session(engine) as session:
