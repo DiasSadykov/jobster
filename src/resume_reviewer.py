616e18df6a4557ca21b68d7c5d4d589112422de9
Dias Sadykov <dias.sadykov0@gmail.com>
2024-06-14 02:03:29 +0300

    lego

--- src/resume_reviewer.py
+++ src/resume_reviewer.py
@@ -1,18 +1,44 @@
-import asyncio
 import os
 import openai
 from PyPDF2 import PdfReader
 from telegram import Update, Bot
 from telegram.ext import Application, MessageHandler, filters, CallbackContext
 
-# Configuration
+ENV = os.environ.get("ENV") or "DEV"
 
-CHATGPT_MODEL = 'gpt-3.5-turbo-0125'
-DOWNLOAD_DIR = '/Users/diassadykov/Desktop'  # Directory to save PDFs temporarily
+# Configuration
+TELEGRAM_TOKEN = os.environ.get("BOT_TOKEN")
+CHATGPT_API_KEY = os.environ.get("CHATGPT_API_KEY")
+CHATGPT_MODEL = 'gpt-4-turbo'
+DOWNLOAD_DIR = '/Users/diassadykov/Desktop' if ENV == "DEV" else "/resumes"  # Directory to save PDFs temporarily
 SYSTEM_PROMPT = """
 Ты карьерный консультант, который помогает людям улучшить свои резюме. 
-Тебе прислали резюме в формате PDF и попросили дать обратную связь. 
+Тебе прислали резюме и попросили дать обратную связь. 
 Пожалуйста, напиши обратную связь на резюме на русском языке.
+В хорошем резюме должны быть следующие разделы:
+1. Контактная информация
+2. Опыт работы
+    1. Название компании
+    2. Должность
+    3. Даты работы
+    4. Достижения в формате XYZ (сделал X, используя Y, что привело к Z), помоги кандидату продемонстрировать свои навыки и достижения в этом формате на примере.
+3. Образование
+4. Личные проекты (если есть)
+    1. Описание проекта
+    2. Технологии, которые использовались
+    3. Результаты (если есть)
+    4. Ссылка на проект
+5. Достижения на олимпиадах или конкурсах (если есть)
+Используй этот формат для обратной связи:
+Привет [Имя], спасибо! Вот мои комментарии по твоему резюме:
+[Положительные аспекты в 1-2 предложениях]
+[Для каждого описания достижеий в опыте работы напиши, как можно улучшить формулировку, чтобы она соответствовала формату XYZ, обязательно упомянай формат XYZ]
+[Исправь раздел с личными проектами, если они не соответствуют формату]
+[Исправь порядок разделов, если это необходимо]
+Пиши что нужно улучшить только в случае если есть что улучшить, иначе пропускай этот пункт.
+Если исправляешь что-то то исправляй на языке оригинала, для этого используй комментарии в формате [Исправлено: Исправленный текст] Если текст на английском то и исправление должно быть на английском.
+Обязательно упомянай формат XYZ в комментариях к достижениям в опыте работы.
+Постарайся уложиться в 1500 символов или меньше.
 """
 
 # Initialize ChatGPT API
@@ -30,8 +56,6 @@ def extract_text_from_pdf(pdf_path):
 
 # Function to get feedback from ChatGPT
 async def get_feedback_from_chatgpt(text):
-    print("Getting feedback from ChatGPT...")
-    print(text)
     response = client.chat.completions.create(
         model=CHATGPT_MODEL,
         messages=[
@@ -55,11 +79,9 @@ async def handle_message(update: Update, context: CallbackContext):
                 await update.message.reply_text("Could not extract text from the PDF.")
                 return
             feedback = await get_feedback_from_chatgpt(text)
-            await update.message.reply_text(f"Feedback from ChatGPT:\n\n{feedback}")
+            await update.message.reply_text(f"{feedback}")
         except Exception as e:
-            await update.message.reply_text(f"Failed to get feedback: {str(e)}")
-        finally:
-            os.remove(file_path)
+            await update.message.reply_text(f"Что-то пошло не так: {str(e)}")
 
 # Main function to set up the bot
 def main():
